ba9b72ff934f58aa96d4f92d8503a86164d908b1
Virgilio Murillo <virgiliomurilloochoa1@gmail.com>
2022-04-01 18:33:39 -0600

    added sql syntax to the project

--- page_ArchWaterLinux/settings.py
+++ page_ArchWaterLinux/settings.py
@@ -75,12 +75,24 @@ WSGI_APPLICATION = 'page_ArchWaterLinux.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+		'default': {
+			'ENGINE' : 'django.db.backends.mysql',
+			'NAME' : "django_ArchWater_DB",
+			'USER' : 'test_django',
+			'PASSWORD' : '' ,
+			'HOST' : 'localhost',
+			'PORT' : ''
+			}
+		}
 
 
 # Password validation
---
+++ test.sql
@@ -0,0 +1,18 @@
+-- Date: 01/April/2022 - Friday
+-- Author: Virgilio Murillo Ochoa
+-- personal github: Virgilio-AI
+-- linkedin: https://www.linkedin.com/in/virgilio-murillo-ochoa-b29b59203
+-- contact: virgiliomurilloochoa1@gmail.com
+-- web: virgiliomurillo.com
+
+
+
+-- create database django_ArchWater_DB ;
+-- use it on the terminal as sudo
+-- sudo mariadb
+grant all privileges on django_ArchWater_DB.* to 'test_django'@localhost ;
+
+
+
+
+
