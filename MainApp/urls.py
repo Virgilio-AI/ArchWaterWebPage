79af1c3b869508dac38bf1cef57a1504aa131260
Virgilio Murillo <virgiliomurilloochoa1@gmail.com>
2022-04-08 18:41:23 -0500

    fixed not finding the files bug

--- MainApp/urls.py
+++ MainApp/urls.py
@@ -4,10 +4,8 @@
 
 from . import views
 from django.urls import path
-from django.conf import settings
 
 
-from django.conf.urls.static import static
 app_name = "main"
 #
 urlpatterns = [
@@ -18,6 +16,3 @@ urlpatterns = [
 	]
 
 
-if settings.DEBUG:
-	urlpatterns+=static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
-	urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
