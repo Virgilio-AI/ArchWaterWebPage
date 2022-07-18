1f13fe5f33aa67da0d2da966640b8a96396dc2b2
Virgilio Murillo <virgiliomurilloochoa1@gmail.com>
2022-07-18 10:59:51 -0500

    fixed the table and the links

--- MainApp/models.py
+++ MainApp/models.py
@@ -5,7 +5,7 @@ from django.db import models
 # Create your models here.
 class IsoFile(models.Model):
 	file = models.FileField(upload_to='media',blank=True)
-	torrent = models.URLField(default="NoLink")
+	torrent = models.CharField(max_length=1000,default="NoLink")
 	name = models.CharField(max_length=100)
 	description = models.CharField(max_length=255)
 	created_at = models.DateTimeField(auto_now_add=True)
